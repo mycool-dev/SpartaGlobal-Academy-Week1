a9034ec2d989c59f9d0c1552128726981d1b40ab
Michael Edie <michaeledie77@outlook.com>
2023-05-06 14:00:39 +0100

    removed test comment

--- src/main/java/com/sparta/me/SOLID/S/Course.java
+++ src/main/java/com/sparta/me/SOLID/S/Course.java
@@ -7,7 +7,6 @@ public class Course {
     private String name;
     private Spartan[] classList;
     private LocalDate startDate;
-    //test comment
 
     public Course(int numberOfTrainees, String name, Spartan[] classList, LocalDate startDate) {
         this.numberOfTrainees = numberOfTrainees;
